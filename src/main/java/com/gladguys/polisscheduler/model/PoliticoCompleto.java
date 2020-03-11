02127b21dfcd96ee5a5edcc27a0b3f3ecbafba5e
denis <dineresc@gmail.com>
2020-03-11 20:09:59 -0300

    add lombok

--- src/main/java/com/gladguys/polisscheduler/model/PoliticoCompleto.java
+++ src/main/java/com/gladguys/polisscheduler/model/PoliticoCompleto.java
@@ -1,5 +1,8 @@
 package com.gladguys.polisscheduler.model;
 
+import lombok.Data;
+
+@Data
 public class PoliticoCompleto {
 
 	private String id;
@@ -9,60 +12,4 @@ public class PoliticoCompleto {
 	private String sexo;
 	private String dataNascimento;
 	private String escolaridade;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNomeCivil() {
-		return nomeCivil;
-	}
-
-	public void setNomeCivil(String nomeCivil) {
-		this.nomeCivil = nomeCivil;
-	}
-
-	public UltimoStatusPolitico getUltimoStatus() {
-		return ultimoStatus;
-	}
-
-	public void setUltimoStatus(UltimoStatusPolitico ultimoStatus) {
-		this.ultimoStatus = ultimoStatus;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getEscolaridade() {
-		return escolaridade;
-	}
-
-	public void setEscolaridade(String escolaridade) {
-		this.escolaridade = escolaridade;
-	}
 }
--- src/main/java/com/gladguys/polisscheduler/model/UltimoStatusPolitico.java
+++ src/main/java/com/gladguys/polisscheduler/model/UltimoStatusPolitico.java
@@ -1,5 +1,8 @@
 package com.gladguys.polisscheduler.model;
 
+import lombok.Data;
+
+@Data
 public class UltimoStatusPolitico {
 
 	private String nome;
@@ -12,86 +15,5 @@ public class UltimoStatusPolitico {
 	private String nomeEleitoral;
 	private String status;
 	private String condicaoEleitoral;
-
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSiglaPartido() {
-		return siglaPartido;
-	}
-
-	public void setSiglaPartido(String siglaPartido) {
-		this.siglaPartido = siglaPartido;
-	}
-
-	public String getUriPartido() {
-		return uriPartido;
-	}
-
-	public void setUriPartido(String uriPartido) {
-		this.uriPartido = uriPartido;
-	}
-
-	public String getSiglaUf() {
-		return siglaUf;
-	}
-
-	public void setSiglaUf(String siglaUf) {
-		this.siglaUf = siglaUf;
-	}
-
-	public String getUrlFoto() {
-		return urlFoto;
-	}
-
-	public void setUrlFoto(String urlFoto) {
-		this.urlFoto = urlFoto;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public String getNomeEleitoral() {
-		return nomeEleitoral;
-	}
-
-	public void setNomeEleitoral(String nomeEleitoral) {
-		this.nomeEleitoral = nomeEleitoral;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getCondicaoEleitoral() {
-		return condicaoEleitoral;
-	}
-
-	public void setCondicaoEleitoral(String condicaoEleitoral) {
-		this.condicaoEleitoral = condicaoEleitoral;
-	}
 }
 
