d8d9f41fb61a8cd65ac18e29353ebe51fbd03cce
denis <dineresc@gmail.com>
2020-03-18 01:01:34 -0300

    cria classes model para busca de despesas da api dados abertos

---
+++ src/main/java/com/gladguys/polisscheduler/model/Despesa.java
@@ -0,0 +1,24 @@
+package com.gladguys.polisscheduler.model;
+
+import lombok.Data;
+
+@Data
+public class Despesa {
+    private String ano;
+    private String cnpjCpfFornecedor;
+    private String codDocumento;
+    private String codLote;
+    private String codTipoDocumento;
+    private String dataDocumento;
+    private String mes;
+    private String nomeFornecedor;
+    private String numDocumento;
+    private String numRessarcimento;
+    private String parcela;
+    private String tipoDespesa;
+    private String tipoDocumento;
+    private String urlDocumento;
+    private String valorDocumento;
+    private String valorGlosa;
+    private String valorLiquido;
+}
