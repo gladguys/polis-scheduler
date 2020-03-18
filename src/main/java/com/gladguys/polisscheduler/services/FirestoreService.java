a3d9daceb37e830a7de5fbf2599c2e59eb8c7290
denis <dineresc@gmail.com>
2020-03-18 01:00:32 -0300

    implementando salvar despesas no firestore

--- src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
+++ src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
@@ -1,7 +1,9 @@
 package com.gladguys.polisscheduler.services;
 
+import com.gladguys.polisscheduler.model.Despesa;
 import com.gladguys.polisscheduler.model.Politico;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -22,8 +25,8 @@ public class FirestoreService {
 		this.db = firestore;
 	}
 
-	//@Scheduled(cron = "0 32 23 * * ?")
-	//@Scheduled(fixedRate = 1000)
+	// @Scheduled(cron = "0 32 23 * * ?")
+	// @Scheduled(fixedRate = 1000)
 	public void getUsers() throws IOException, ExecutionException, InterruptedException {
 
 		ApiFuture<QuerySnapshot> query = db.collection("users").get();
@@ -36,5 +39,22 @@ public class FirestoreService {
 
 	public void addPolitico(Politico politico) {
 		ApiFuture<WriteResult> future = db.collection("politicos").document(politico.getId()).set(politico);
+	}
+
+	public List<String> getPoliticosIds() throws InterruptedException, ExecutionException {
+		List<String> politicosIds = new ArrayList<>();
+		ApiFuture<QuerySnapshot> future = db.collection("politicos").get();
+		List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+		for (DocumentSnapshot document : documents) {
+			politicosIds.add(document.getId());	
+		}
+		
+		return politicosIds;
+	}
+
+	public void salvarDespesas(List<Despesa> despesas, String politicoId) {
+		despesas.forEach(d -> {
+			db.collection("atividades").document(politicoId).collection("despesasPolitico").add(d); 
+		});
 	}
 }
