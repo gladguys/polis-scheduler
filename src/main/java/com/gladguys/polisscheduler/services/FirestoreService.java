358147b5fee98935190eff7b9df27dfab0446fda
denis <dineresc@gmail.com>
2020-04-09 01:27:32 -0300

    cria logica de hash para politicos

--- src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
+++ src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
@@ -106,4 +106,8 @@ public class FirestoreService {
 		db.collection("sync_log").document("PARTIDOSYNC").update("hash", hash);
 	}
 
+	public void updateHashCodeSyncPoliticos() {
+		String hash = LocalDateTime.now().toString();
+		db.collection("sync_log").document("POLITICOSYNC").update("hash", hash);
+	}
 }
