75310bbcc1a7c9d45052952fede7cf3b70726d66
denis <dineresc@gmail.com>
2020-04-18 09:25:28 -0300

    melhroando separacao de codigo para firestore

--- src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
+++ src/main/java/com/gladguys/polisscheduler/services/FirestoreService.java
@@ -1,22 +1,9 @@
 package com.gladguys.polisscheduler.services;
 
-import com.gladguys.polisscheduler.model.Partido;
-import com.gladguys.polisscheduler.model.Politico;
-import com.gladguys.polisscheduler.model.Proposicao;
-import com.gladguys.polisscheduler.model.Tramitacao;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
-
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 @Service
 public class FirestoreService {
@@ -25,26 +12,6 @@ public class FirestoreService {
 
 	public FirestoreService(Firestore firestore) {
 		this.db = firestore;
-	}
-
-	public void addPolitico(Politico politico) {
-		db.collection("politicos").document(politico.getId()).set(politico);
-	}
-
-	public List<Politico> getPoliticos() throws InterruptedException, ExecutionException {
-		List<Politico> politicos = new ArrayList<>();
-		ApiFuture<QuerySnapshot> future = db.collection("politicos").get();
-		List<QueryDocumentSnapshot> documents = future.get().getDocuments();
-		for (DocumentSnapshot document : documents) {
-			politicos.add(document.toObject(Politico.class));
-		}
-		return politicos;
-	}
-
-	public void salvarPartidos(List<Partido> partidos) {
-		partidos.forEach(p -> {
-			db.collection("partidos").document(p.getId()).set(p);
-		});
 	}
 
 	public void updateHashCodeSyncPartidos() {
