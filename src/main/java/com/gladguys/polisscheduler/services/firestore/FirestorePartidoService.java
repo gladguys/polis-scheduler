a47e5652efdc66529c70fe961c1e007b2fe9c260
denis <dineresc@gmail.com>
2020-05-07 14:49:15 -0300

    refaz metodo de busca partido

--- src/main/java/com/gladguys/polisscheduler/services/firestore/FirestorePartidoService.java
+++ src/main/java/com/gladguys/polisscheduler/services/firestore/FirestorePartidoService.java
@@ -1,12 +1,10 @@
 package com.gladguys.polisscheduler.services.firestore;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import com.gladguys.polisscheduler.model.Partido;
-import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 
 import org.springframework.stereotype.Service;
@@ -16,24 +14,32 @@ public class FirestorePartidoService {
 
     private final Firestore db;
 
-	public FirestorePartidoService(Firestore firestore) {
-		this.db = firestore;
-	}
+    public FirestorePartidoService(Firestore firestore) {
+        this.db = firestore;
+    }
 
-	public void salvarPartidos(List<Partido> partidos) {
-		partidos.forEach(p -> {
-			db.collection("partidos").document(p.getId()).update(p.parseToMap());
-		});
-	}
+    public void salvarPartidos(List<Partido> partidos) {
+        partidos.forEach(p -> {
+            db.collection("partidos").document(p.getId()).update(p.parseToMap());
+        });
+    }
 
-	public Partido getById(String partidoId) {
-		var partido = db.collection("partidos").document(partidoId).get();
-		try {
-			return partido.get().toObject(Partido.class);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-	}
+    public Optional<Partido> getById(String partidoSigla) {
+        try {
+            var partido =
+                    db.collection("partidos")
+                            .whereEqualTo("sigla", partidoSigla)
+                            .get()
+                            .get()
+                            .getDocuments()
+                            .get(0);
+
+            return Optional.of(partido.toObject(Partido.class));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
