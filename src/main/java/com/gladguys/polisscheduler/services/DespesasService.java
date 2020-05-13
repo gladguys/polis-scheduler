90f7185d93ec0b8e18557f6ea6cdc167c6a3a7af
denis <dineresc@gmail.com>
2020-05-12 22:47:13 -0300

    criando servicos para despesa

--- src/main/java/com/gladguys/polisscheduler/services/DespesasService.java
+++ src/main/java/com/gladguys/polisscheduler/services/DespesasService.java
@@ -1,6 +1,8 @@
 package com.gladguys.polisscheduler.services;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
@@ -75,6 +77,28 @@ public class DespesasService {
                     despesasDeHoje.stream().filter(d -> d.getDataDocumento() != null).collect(Collectors.toList()),
                     p.getId());
         });
+    }
+
+    public void totalizadorDespesasPorAnoEmes(String ano, String mes) throws ExecutionException, InterruptedException {
+
+        //pega todos os deputados da base
+        List<Politico> politicos = firestorePoliticoService.getPoliticos();
+        politicos.parallelStream().forEach(politico -> {
+            String urlParaDespesasPolitico = URI_POLITICOS + politico.getId() + "/despesas?ano="+ano+"&mes=" + mes
+                    + "&pagina=1&itens=100000000&ordem=ASC&ordenarPor=ano";
+
+            List<Despesa> despesas = this.restTemplate
+                    .getForObject(urlParaDespesasPolitico, RetornoDespesas.class).getDados();
+
+            Optional<BigDecimal> valorMes = despesas.parallelStream()
+                    .map(d -> new BigDecimal(d.getValorLiquido()))
+                    .reduce((v1, v2) -> v1.add(v2));
+
+            firestoreService.salvarTotalDespesaPoliticoPorMes(politico.getId(),mes,valorMes.orElse(new BigDecimal(0.0)));
+
+        });
+        //get despesas por ano e mes para cada deputado
+
     }
 
     public String criarDespesaMock() {
--- src/main/java/com/gladguys/polisscheduler/services/firestore/FirestoreDespesaService.java
+++ src/main/java/com/gladguys/polisscheduler/services/firestore/FirestoreDespesaService.java
@@ -1,6 +1,9 @@
 package com.gladguys.polisscheduler.services.firestore;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.gladguys.polisscheduler.model.Despesa;
@@ -13,36 +16,46 @@ public class FirestoreDespesaService {
 
     private final Firestore db;
 
-	public FirestoreDespesaService(Firestore firestore) {
-		this.db = firestore;
+    public FirestoreDespesaService(Firestore firestore) {
+        this.db = firestore;
     }
-    
+
     public void salvarDespesas(List<Despesa> despesas, String politicoId) {
-		try {
-			despesas.forEach(d -> {
-				db.collection("atividades")
-						.document(politicoId)
-						.collection("atividadesPolitico")
-						.document(montaIdDespesa(d))
-						.create(d);
-			});
-		} catch (Exception e) {
-			System.err.println(e);
-		}
-	}
+        try {
+            despesas.forEach(d -> {
+                db.collection("atividades")
+                        .document(politicoId)
+                        .collection("atividadesPolitico")
+                        .document(montaIdDespesa(d))
+                        .create(d);
+            });
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 
-	public String salvarDespesa(Despesa despesa) {
-		var despesaId = UUID.randomUUID().toString();
-		db.collection("atividades")
-				.document(despesa.getIdPolitico())
-				.collection("atividadesPolitico")
-				.document(despesaId)
-				.create(despesa);
-		return despesaId;
-	}
+    public String salvarDespesa(Despesa despesa) {
+        var despesaId = UUID.randomUUID().toString();
+        db.collection("atividades")
+                .document(despesa.getIdPolitico())
+                .collection("atividadesPolitico")
+                .document(despesaId)
+                .create(despesa);
+        return despesaId;
+    }
 
-	private String montaIdDespesa(Despesa d) {
-		return d.getDataDocumento().replace("-", "") + d.getIdPolitico()
-				+ d.getValorDocumento().replace(".", "") + d.getCodDocumento();
-	}
+    private String montaIdDespesa(Despesa d) {
+        return d.getDataDocumento().replace("-", "") + d.getIdPolitico()
+                + d.getValorDocumento().replace(".", "") + d.getCodDocumento();
+    }
+
+    public void salvarTotalDespesaPoliticoPorMes(String id, String mes, BigDecimal totalDespesas) {
+        Map<String, Object> mesValor = new HashMap<>();
+        mesValor.put("total", totalDespesas.doubleValue());
+        db.collection("totalizador_despesas")
+                .document(id)
+                .collection("totalPorMes")
+                .document(mes)
+                .set(mesValor);
+    }
 }
